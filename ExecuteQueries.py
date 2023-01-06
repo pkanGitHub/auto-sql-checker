c6549b5948fee3c2b1c2f82b145a2eb91fbbc1eb
Phyllis Kan <pyk9q7@gmail.com>
2023-01-05 21:56:22 -0600

    output each query results onto xlsx sheets

--- ExecuteQueries.py
+++ ExecuteQueries.py
@@ -1,8 +1,9 @@
-import mysql.connector
 import pandas as pd
+# import numpy as np
 from FormatQueries import *
 from sqlalchemy import create_engine
-import os
+from openpyxl import Workbook
+from openpyxl.styles import Border, Alignment, PatternFill
 
 def get_database():
     print("\nAvailable databases: ")
@@ -31,29 +32,49 @@ def create_connector_object(database_name):
 
     return connection
 
+def create_workbook(path):
+   workbook = Workbook()
+   workbook.save(path) 
 
-def read_formatted_file(database):
+def cell_style(path):
+    workbook = Workbook()
+    sheet = workbook.active
+    # for wrapRows in sheet.iter_rows(min_row=1, min_col=1):
+    #     for cell in wrapRows:
+    #         cell.alignment = Alignment(horizontal="center", vertical="center", wrapText=True)
+    sheet.column_dimensions
+
+    # green = "96C8A2"
+    # for colorRow in sheet.head(1):
+    #     for colorCell in colorRow:
+    #         colorCell.fill = PatternFill(start_color=green, end_color=green,fill_type = "solid")
+    workbook.save(path)
+
+
+def read_formatted_file(database, path):
     mydb = create_connector_object(database)
-    # mycursor = mydb.cursor()
     with open ('FormattedQueries.sql') as file:
-        queries_file = file.readline()
-        pd_query = pd.read_sql(queries_file, mydb)
-        try:
-            # mycursor.execute(queries_file)
-            # result = mycursor.fetchall()
-            # for query in result:
-            #     print(query)
-                
-            df = pd.DataFrame(pd_query)
-            df.to_csv(r'Result_Data.csv', index = False)
-            print('Successfully write to csv file')
-        except Exception as err:
-            print(f"Error Occured: {err}")
+        queries_file = file.read()
+        queries = queries_file.splitlines()
+        page = 1
+        for q in queries:
+            pd_query = pd.read_sql(q, mydb)
+            try:
+                df = pd.DataFrame(pd_query)
+                with pd.ExcelWriter(path,mode="a",engine="openpyxl",if_sheet_exists="overlay") as writer:
+                    # df.to_excel(writer,sheet_name="Sheet",startrow=writer.sheets["Sheet"].max_row,index=False)
+                    df.to_excel(writer,sheet_name=f"Sheet{page}", index=False)
+                    print(f'Query {page} write successfully')
+                    page += 1
+            except Exception as err:
+                print(f"Error Occured: {err}")
 
 def main():
+    path = 'Result_Data.xlsx'
     writeFile()
+    create_workbook(path) 
     database = get_database()
-    read_formatted_file(database)
+    read_formatted_file(database, path)
     try:
         create_connector_object(database)
         print("Successfully connected to the database!")
